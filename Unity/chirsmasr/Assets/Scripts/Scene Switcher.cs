53b21918da98ba944844d1957bfea346d6a9f5af
Dylan Davis <davis948@student.pcsstn.net>
2025-12-12 00:05:53 -0600

    please

---
+++ Unity/chirsmasr/Assets/Scripts/Scene Switcher.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Control : MonoBehaviour
+{
+    
+    public void NextScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
---
+++ Unity/chirsmasr/Assets/Scripts/test.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class test : MonoBehaviour
+{
+    public string scene;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void loadthat()
+    {
+        
+        SceneManager.LoadScene(scene);
+    }
+}
